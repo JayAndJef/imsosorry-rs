1341134d85ba9fdab978e8b542167572f0bbd67b
JayAndJef <109392310+JayAndJef@users.noreply.github.com>
2023-08-27 16:34:14 -0700

    repace with w

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use regex::Regex;
 
 pub fn uwuify(text: &str, stutter_strength: Option<f32>, emoji_strength: Option<f32>, tilde_strength: Option<f32>) {
     todo!()
@@ -27,6 +28,12 @@ fn replace_word(word: &str) -> &str {
     }
 }
 
+fn replace_with_w(text: &str) -> String {
+    let char_regex = Regex::new("[lr]").unwrap();
+
+    char_regex.replace_all(text, "w").to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,5 +47,11 @@ mod tests {
     fn text_word_replace() {
         let replaces_string = words_replace("small cute fluff love stupid idiot what meow roar");
         assert_eq!(replaces_string, "smol kawaii~ floof luv baka baka nani nya~ rawrr~")
+    }
+
+    #[test]
+    fn test_replace_with_w() {
+        let w_replace = replace_with_w("rowdy rowers are great");
+        assert_eq!(w_replace, "wowdy wowews awe gweat");
     }
 }
